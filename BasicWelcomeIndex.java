d4375d20c2e909aa7ce040a61920074135766c2e
mrunal08 <mrunalg96@gmail.com>
2021-08-18 14:03:34 +0530

    First Commit

---
+++ BasicWelcomeIndex.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class BasicWelcomeIndex {
+    public static void main(String[] args){
+        System.out.println("Welcome to Logical Programs");
+       
+    }
+}
